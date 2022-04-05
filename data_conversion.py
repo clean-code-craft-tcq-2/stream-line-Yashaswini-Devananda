7339529b657c5e1e6a0ff92dba070a355ce527f0
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-05 23:10:03 +0530

    Update data_conversion.py

--- data_conversion.py
+++ data_conversion.py
@@ -1,7 +1,7 @@
 ADC_12Bit = {"max_permissible_value" : 4094 , "intercept" : 0 , "max_current_range" : 10}
 
 def map_adc_value_to_amps(A2Dvalue, ADC_dict):
-  result = 0
+  value = 0
   value -= ADC_dict["intercept"]
   if value<=ADC_dict["max_permissible_value"] :
     result = abs(round(((value)/ADC_dict["max_permissible_value"])*ADC_dict["max_current_range"]))
