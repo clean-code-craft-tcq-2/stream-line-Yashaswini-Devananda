d3eb6e30380b40b54b407c54285fa3ee6611c6ac
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-06 23:00:51 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -15,21 +15,27 @@ def generate_sensor_readings(min_value, max_value):
 
 def send_data_to_receiver(data):
     sys.stdout.write(data)
-    sys.stdout.write("\n")
+#     sys.stdout.write("\n")
     return (sys.stdout.write(data))
     
-def send_sensor_data(A2D_value, temperature_value):
+def process_sensor_data(A2D_value, temperature_value):
     current = data_conversion.map_adc_value_to_amps(A2D_value, ADC_12Bit)
     temperature = data_conversion.map_celsius_to_fahrenheit(temperature_value)
-    data = "current in amps : " + current + "," + "temperature in Fahrenheit : " + temperature
-    return 
+    data = "current in amps : " + current + "," + "temperature in Fahrenheit : " + temperature + "\n"
+    return data
     
-for reading in range (streaming_data_limit):
-    A2D_value = generate_sensor_readings(min_A2D_value, max_A2D_value)
-    temperature = generate_sensor_readings(min_temp_value, max_temp_value)
-    send_sensor_data(A2D_value, temperature)
-    time.sleep(1)
+def send_readings():
+    number_of_readings_sent = 0
+    for reading in range (streaming_data_limit):
+        number_of_readings_sent += 1
+        A2D_value = generate_sensor_readings(min_A2D_value, max_A2D_value)
+        temperature = generate_sensor_readings(min_temp_value, max_temp_value)
+        data = process_sensor_data(A2D_value, temperature)
+        time.sleep(0.5)
+    return number_of_readings_sent
     
+if __name__ == "__main__":
+    number_of_readings_sent = send_readings()
     
 
     
