e46dc3713eb7350e021f18a444addf0000821745
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-06 22:45:30 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -12,13 +12,17 @@ ADC_12Bit = {"max_permissible_value" : 4094 , "intercept" : 0 , "max_current_ran
 
 def generate_sensor_readings(min_value, max_value):
     return(round(random.randint(min_value, max_value)))
+
+def send_data_to_receiver(data):
+    sys.stdout.write(data)
+    sys.stdout.write("\n")
+    return (sys.stdout.write(data))
     
 def send_sensor_data(A2D_value, temperature_value):
     current = data_conversion.map_adc_value_to_amps(A2D_value, ADC_12Bit)
     temperature = data_conversion.map_celsius_to_fahrenheit(temperature_value)
-    # print ((current + "," + temperature))
-    sys.stdout.write(current + "," + temperature)
-    sys.stdout.write("\n")
+    data = "current in amps : " + current + "," + "temperature in Fahrenheit : " + temperature
+    return 
     
 for reading in range (streaming_data_limit):
     A2D_value = generate_sensor_readings(min_A2D_value, max_A2D_value)
