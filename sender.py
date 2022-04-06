8ad18f4cd34db65f75e96991c8f960342620dc13
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-06 23:07:03 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,9 +1,50 @@
+# import sys
+# import time
+# import random
+# import data_conversion
+
+# streaming_data_limit = 5
+# max_temp_value = 110.0
+# min_temp_value = 0.0
+# max_A2D_value = 4094.0
+# min_A2D_value =  0.0
+# ADC_12Bit = {"max_permissible_value" : 4094 , "intercept" : 0 , "max_current_range" : 10}
+
+# def generate_sensor_readings(min_value, max_value):
+#     return(round(random.randint(min_value, max_value)))
+
+# def send_data_to_receiver(data):
+#     sys.stdout.write(data)
+# #     sys.stdout.write("\n")
+#     return (sys.stdout.write(data))
+    
+# def process_sensor_data(A2D_value, temperature_value):
+#     current = data_conversion.map_adc_value_to_amps(A2D_value, ADC_12Bit)
+#     temperature = data_conversion.map_celsius_to_fahrenheit(temperature_value)
+#     data = "current in amps : " + current + "," + "temperature in Fahrenheit : " + temperature + "\n"
+#     return data
+    
+# def send_readings():
+#     number_of_readings_sent = 0
+#     for reading in range (streaming_data_limit):
+#         number_of_readings_sent += 1
+#         A2D_value = generate_sensor_readings(min_A2D_value, max_A2D_value)
+#         temperature = generate_sensor_readings(min_temp_value, max_temp_value)
+#         data = process_sensor_data(A2D_value, temperature)
+#         time.sleep(0.5)
+#     return number_of_readings_sent
+    
+# if __name__ == "__main__":
+#     number_of_readings_sent = send_readings()
+
+
 import sys
 import time
 import random
 import data_conversion
 
-streaming_data_limit = 5
+
+streaming_data_limit = 1
 max_temp_value = 110.0
 min_temp_value = 0.0
 max_A2D_value = 4094.0
@@ -12,30 +53,41 @@ ADC_12Bit = {"max_permissible_value" : 4094 , "intercept" : 0 , "max_current_ran
 
 def generate_sensor_readings(min_value, max_value):
     return(round(random.randint(min_value, max_value)))
-
-def send_data_to_receiver(data):
-    sys.stdout.write(data)
-#     sys.stdout.write("\n")
-    return (sys.stdout.write(data))
     
-def process_sensor_data(A2D_value, temperature_value):
-    current = data_conversion.map_adc_value_to_amps(A2D_value, ADC_12Bit)
+def map_adc_value_to_amps(A2Dvalue, ADC_dict):
+    result = ""
+    value = A2Dvalue
+    value -= ADC_dict["intercept"]
+    if value<=ADC_dict["max_permissible_value"] :
+        result = str(abs(round(((value)/ADC_dict["max_permissible_value"])*ADC_dict["max_current_range"])))
+    return (result)
+  
+def map_celsius_to_fahrenheit(temperature):
+    temperature_in_fahrenheit = ((temperature * 9/5) + 32)
+    return str(round(temperature_in_fahrenheit))
+    
+def send_sensor_data(A2D_value, temperature_value):
+    current = data_conversion.map_adc_value_to_amps(A2D_value, data_conversion.ADC_12Bit)
     temperature = data_conversion.map_celsius_to_fahrenheit(temperature_value)
-    data = "current in amps : " + current + "," + "temperature in Fahrenheit : " + temperature + "\n"
-    return data
+    # print ((current + "," + temperature))
+    sys.stdout.write(current + "," + temperature)
+    sys.stdout.write("\n")
     
-def send_readings():
-    number_of_readings_sent = 0
-    for reading in range (streaming_data_limit):
-        number_of_readings_sent += 1
-        A2D_value = generate_sensor_readings(min_A2D_value, max_A2D_value)
-        temperature = generate_sensor_readings(min_temp_value, max_temp_value)
-        data = process_sensor_data(A2D_value, temperature)
-        time.sleep(0.5)
-    return number_of_readings_sent
+for reading in range (streaming_data_limit):
+    A2D_value = generate_sensor_readings(min_A2D_value, max_A2D_value)
+    temperature = generate_sensor_readings(min_temp_value, max_temp_value)
+#     print (A2D_value)
+#     print (temperature)
+    send_sensor_data(A2D_value, temperature)
+    time.sleep(1)
     
-if __name__ == "__main__":
-    number_of_readings_sent = send_readings()
+    
+
+    
+    
+
+
+
     
 
     
