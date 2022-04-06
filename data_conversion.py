dff284778c7cd043494cb16a8c453977382412d4
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-06 19:32:25 +0530

    Update data_conversion.py

--- data_conversion.py
+++ data_conversion.py
@@ -1,11 +1,13 @@
 ADC_12Bit = {"max_permissible_value" : 4094 , "intercept" : 0 , "max_current_range" : 10}
 
 def map_adc_value_to_amps(A2Dvalue, ADC_dict):
-  value = 0
-  value -= ADC_dict["intercept"]
-  if value<=ADC_dict["max_permissible_value"] :
-    result = abs(round(((value)/ADC_dict["max_permissible_value"])*ADC_dict["max_current_range"]))
-  return result
+    result = ""
+    value = A2Dvalue
+    value -= ADC_dict["intercept"]
+    if value<=ADC_dict["max_permissible_value"] :
+        result = str(abs(round(((value)/ADC_dict["max_permissible_value"])*ADC_dict["max_current_range"])))
+    return (result)
   
 def map_celsius_to_fahrenheit(temperature):
-  return ((temperature * 9/5) + 32)
+    temperature_in_fahrenheit = ((temperature * 9/5) + 32)
+    return str(round(temperature_in_fahrenheit))
