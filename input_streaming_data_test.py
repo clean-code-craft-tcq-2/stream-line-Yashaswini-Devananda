98b33d3db26be3f41fca9cbe64a5043e2c63e7fd
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-06 20:11:42 +0530

    Update input_streaming_data_test.py

--- input_streaming_data_test.py
+++ input_streaming_data_test.py
@@ -6,5 +6,5 @@ A2D_values = [0,4000,0,56,4025,9,900,2500,3000,]
 temperature_value = [2,10,2,15,25,35,5,63,45]
 
 for x in range (len(A2D_values)):
-    sender.send_sensor_data(A2D_values[x], temperature_value[x])
+    sender.streaming_sensor_data(A2D_values[x], temperature_value[x])
     time.sleep(1)
