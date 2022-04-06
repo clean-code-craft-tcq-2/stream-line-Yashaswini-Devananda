06cb59ceb60ac931cb5764709c4d7158a42a384e
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-06 23:30:05 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -16,15 +16,12 @@ def generate_sensor_readings(min_value, max_value):
 def send_sensor_data(A2D_value, temperature_value):
     current = data_conversion.map_adc_value_to_amps(A2D_value, data_conversion.ADC_12Bit)
     temperature = data_conversion.map_celsius_to_fahrenheit(temperature_value)
-    # print ((current + "," + temperature))
     sys.stdout.write(current + "," + temperature)
     sys.stdout.write("\n")
     
 for reading in range (streaming_data_limit):
     A2D_value = generate_sensor_readings(min_A2D_value, max_A2D_value)
     temperature = generate_sensor_readings(min_temp_value, max_temp_value)
-#     print (A2D_value)
-#     print (temperature)
     send_sensor_data(A2D_value, temperature)
     time.sleep(1)
     
