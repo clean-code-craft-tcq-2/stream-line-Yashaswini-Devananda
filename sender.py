00f49b29d3d283fc6c2ecac3f963a772fb304d2d
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-06 19:40:52 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,8 +1,8 @@
 import sys
+import data_conversion
 
 def streaming_sensor_data(A2D_value, temperature_value):
-    current = map_adc_value_to_amps(A2D_value, ADC_12Bit)
-    temperature = map_celsius_to_fahrenheit(temperature_value)
-    # print ((current + "," + temperature))
+    current = data_conversion.map_adc_value_to_amps(A2D_value, data_conversion.ADC_12Bit)
+    temperature = data_conversion.map_celsius_to_fahrenheit(temperature_value)
     sys.stdout.write(current + "," + temperature)
     sys.stdout.write("\n")
