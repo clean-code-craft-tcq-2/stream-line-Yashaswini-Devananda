81dbf95b5b77d801b5e04c4bae19f853d72d6a89
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-06 19:36:06 +0530

    Create streaming_input.py

---
+++ streaming_input.py
@@ -0,0 +1,6 @@
+def send_sensor_data(A2D_value, temperature_value):
+    current = map_adc_value_to_amps(A2D_value, ADC_12Bit)
+    temperature = map_celsius_to_fahrenheit(temperature_value)
+    # print ((current + "," + temperature))
+    sys.stdout.write(current + "," + temperature)
+    sys.stdout.write("\n")
