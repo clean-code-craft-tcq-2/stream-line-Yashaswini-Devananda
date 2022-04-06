3ba155a8ab2ea126db941d91ec0adda444ad73b3
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-06 23:11:12 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -3,7 +3,6 @@ import time
 import random
 import data_conversion
 
-
 streaming_data_limit = 1
 max_temp_value = 110.0
 min_temp_value = 0.0
@@ -11,17 +10,6 @@ max_A2D_value = 4094.0
 min_A2D_value =  0.0
 ADC_12Bit = {"max_permissible_value" : 4094 , "intercept" : 0 , "max_current_range" : 10}
 
-def generate_sensor_readings(min_value, max_value):
-    return(round(random.randint(min_value, max_value)))
-    
-def map_adc_value_to_amps(A2Dvalue, ADC_dict):
-    result = ""
-    value = A2Dvalue
-    value -= ADC_dict["intercept"]
-    if value<=ADC_dict["max_permissible_value"] :
-        result = str(abs(round(((value)/ADC_dict["max_permissible_value"])*ADC_dict["max_current_range"])))
-    return (result)
-  
 def map_celsius_to_fahrenheit(temperature):
     temperature_in_fahrenheit = ((temperature * 9/5) + 32)
     return str(round(temperature_in_fahrenheit))
