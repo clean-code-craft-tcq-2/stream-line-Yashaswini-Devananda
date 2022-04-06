e5bd945b4ef60caaa50ed9152cb4d0928b780008
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-06 19:37:14 +0530

    Update and rename streaming_input.py to sender.py

--- sender.py
+++ sender.py
@@ -1,4 +1,4 @@
-def send_sensor_data(A2D_value, temperature_value):
+def streaming_sensor_data(A2D_value, temperature_value):
     current = map_adc_value_to_amps(A2D_value, ADC_12Bit)
     temperature = map_celsius_to_fahrenheit(temperature_value)
     # print ((current + "," + temperature))
