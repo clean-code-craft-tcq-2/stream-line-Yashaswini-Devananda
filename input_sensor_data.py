c6a6f5eb585f719e3e34d5f49620083d241deb39
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-07 09:04:22 +0530

    Update input_sensor_data.py

--- input_sensor_data.py
+++ input_sensor_data.py
@@ -1,6 +1,6 @@
-A2D_readings = [0,4000]
-temperature_sensor_readings = [0,1]
+A2D_readings = [0,4000] # 12 bit A2D readings 
+temperature_sensor_readings = [0,1] # temperature in celsius
 
-data_sent_to_receiver = ["0,32", "10,34"]
+data_sent_to_receiver = ["0,32", "10,34"] # current_in_amperes,temperature_in_Fahrenheit
 
 
