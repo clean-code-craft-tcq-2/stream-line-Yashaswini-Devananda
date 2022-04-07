ea1fcb96ccb79c9c7c4bcc961546e16dfa297b21
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-07 08:10:33 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -11,7 +11,6 @@ def send_sensor_data(A2D_value, temperature_value):
     current = data_conversion.map_adc_value_to_amps(A2D_value, data_conversion.ADC_12Bit)
     temperature = data_conversion.map_celsius_to_fahrenheit(temperature_value)
     sys.stdout.write(current + "," + temperature + "\n")
-#     sys.stdout.write("\n")
     
 for reading in range (streaming_data_limit):
     send_sensor_data(A2D_readings[reading], temperature_readings[reading])
