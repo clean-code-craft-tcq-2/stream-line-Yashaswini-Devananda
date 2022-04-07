b4ab0444a313549092ec26ea3fba5d08bda67345
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-07 08:48:58 +0530

    Update input_sensor_data.py

--- input_sensor_data.py
+++ input_sensor_data.py
@@ -1,7 +1,7 @@
 A2D_readings = [0,4000]
 temperature_sensor_readings = [0,1]
 
-data_sent_to_receiver = ["0,32"+"\n", "10,34"+"\n"]
+data_sent_to_receiver = ["0,32", "10,34"]
 
 for x in data_sent_to_receiver:
-  print x
+  print (x)
