8e097b1a7ea9b34b9d73f0b3ce8db78a5d1187e1
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-07 09:25:33 +0530

    Update streaming_data_test.py

--- streaming_data_test.py
+++ streaming_data_test.py
@@ -14,12 +14,12 @@ class input_sensor_data_test(unittest.TestCase):
   def test(self):
     data_to_be_sent = ""
     for x in input_sensor_data.data_sent_to_receiver:
-      data_to_be_sent += x
+      data_to_be_sent = data_to_be_sent + x
       print ("data_to_be_sent")
       print (data_to_be_sent)
       print ("Data_received")
-      print (received_data)
-      self.assertTrue(received_data == data_to_be_sent)
+      print (received_data[0])
+    self.assertTrue(received_data[0] == data_to_be_sent)
         
 unittest.main()
   
