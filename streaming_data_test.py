e4001e9083fff3749072e0791e51625c7530f2a6
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-07 09:16:11 +0530

    Update streaming_data_test.py

--- streaming_data_test.py
+++ streaming_data_test.py
@@ -9,14 +9,17 @@ received_data.append(sys.stdin.read())
 print ("Data_received")
 print (received_data)
 
-# class input_sensor_data_test(unittest.TestCase):
+class input_sensor_data_test(unittest.TestCase):
   
-#   def test(self):
-#     for reading in range (sender.streaming_data_limit):
-#       received_data = sys.stdin.read()
-#       print (received_data)
-#       print (input_sensor_data.data_sent_to_receiver[reading])
-#       self.assertTrue(input_sensor_data.data_sent_to_receiver[reading] == received_data)
+  def test(self):
+    data_to_be_sent = ""
+    for x in input_sensor_data.data_sent_to_receiver:
+      data_to_be_sent += x
+      print ("data_to_be_sent")
+      print (data_to_be_sent)
+      print ("Data_received")
+      print (received_data)
+      self.assertTrue(received_data == data_to_be_sent)
         
-# unittest.main()
+unittest.main()
   
