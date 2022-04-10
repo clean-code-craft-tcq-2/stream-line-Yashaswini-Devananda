ac8ded421875d664232f27ed819e852624029560
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-10 12:15:00 +0530

    Delete input_sensor_data.py

--- input_sensor_data.py
+++
@@ -1,6 +0,0 @@
-A2D_readings = [0,4000] # 12 bit A2D readings 
-temperature_sensor_readings = [0,1] # temperature in celsius
-
-data_sent_to_receiver = ["0,32\n", "10,34\n"] # current_in_amperes,temperature_in_Fahrenheit
-
-
