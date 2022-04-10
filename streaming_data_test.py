8317ad569474f710575282a8c33687d93ec589fb
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-10 15:12:43 +0530

    Update streaming_data_test.py

--- streaming_data_test.py
+++ streaming_data_test.py
@@ -5,20 +5,23 @@ import unittest
 import input_sensor_data
 import unittest
 
-A2D_readings = [0,0]
-temperature_sensor_readings = [0,0]
-# data_received.append(sys.stdin.read())
-data1 = sender.start_sending(A2D_readings,temperature_sensor_readings)
-# print (sender.start_sending(A2D_readings,temperature_sensor_readings))
-print (data1)
+# A2D_readings = [0,0]
+# temperature_sensor_readings = [0,0]
+# # data_received.append(sys.stdin.read())
+# data1 = sender.start_sending(A2D_readings,temperature_sensor_readings)
+# # print (sender.start_sending(A2D_readings,temperature_sensor_readings))
+# print (data1)
 
-# class sender_test(unittest.TestCase):
+class sender_test(unittest.TestCase):
   
-#   def test(self):
-#     for x in range(len(input_sensor_data.test_A2D_readings)):
-#       self.assertEqual(sender.start_sending(input_sensor_data.test_A2D_readings[x],input_sensor_data.test_temperature_readings[x]), input_sensor_data.result_data[x] , "not equal")
+  def test(self):
+    for x in range(len(input_sensor_data.test_A2D_readings)):
+      print("-------------")
+      print(sender.start_sending(input_sensor_data.test_A2D_readings[x],input_sensor_data.test_temperature_readings[x]))
+      print("-------------")
+      self.assertEqual(sender.start_sending(input_sensor_data.test_A2D_readings[x],input_sensor_data.test_temperature_readings[x]), input_sensor_data.result_data[x] , "not equal")
         
-# unittest.main()
+unittest.main()
   
   
   
