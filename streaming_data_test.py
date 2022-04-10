4de9ca31b510db2f254fec1be8ade8e1fcdadc03
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-10 13:19:36 +0530

    Update streaming_data_test.py

--- streaming_data_test.py
+++ streaming_data_test.py
@@ -4,28 +4,11 @@ import sender
 import unittest
 import input_sensor_data
 
-data_received = []
+A2D_readings = [4000, 4000]
+temperature_sensor_readings = [1,1]
+# data_received.append(sys.stdin.read())
 
-data_received.append(sys.stdin.read())
+print (start_sending(A2D_readings,temperature_readings))
 
-print (data_received)
 
-# received_data = []
-# received_data.append(sys.stdin.read())
-# print ("Data_received")
-# print (received_data)
 
-# class input_sensor_data_test(unittest.TestCase):
-  
-#   def test(self):
-#     data_to_be_sent = ""
-#     for x in input_sensor_data.data_sent_to_receiver:
-#       data_to_be_sent = data_to_be_sent + x
-#       print ("data_to_be_sent")
-#       print (data_to_be_sent)
-#       print ("Data_received")
-#       print (received_data[0])
-#     self.assertTrue(received_data[0] == data_to_be_sent)
-        
-# unittest.main()
-  
