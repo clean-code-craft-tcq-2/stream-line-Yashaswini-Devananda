ce186d11e9f5292cf2de493c879e1b0669894d47
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-10 16:15:07 +0530

    Update input_sensor_data.py

--- input_sensor_data.py
+++ input_sensor_data.py
@@ -1,4 +1,7 @@
-test_A2D_readings = [[0, 4000]]
-test_temperature_readings = [[0,1]]
+test_A2D_readings = [[0, 4000],
+                     [1,2,10,500,700,1000,2000,3000,4000,1025],]
+test_temperature_readings = [[0,1],
+                             [2,5,6,15,52,6,45,9,11,40],]
 
-result_data = ["0,32\n10,34\n"]
+result_data = ["0,32\n10,34\n" ,
+               "0,36\n0,41\n0,43\n1,59\n2,126\n2,43\n5,113\n7,48\n10,52\n3,104\n",]
