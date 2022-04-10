999e6c8c15d94821f02ee0ca3d2377ca0dfa4b2b
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-10 15:57:46 +0530

    Update streaming_data_test.py

--- streaming_data_test.py
+++ streaming_data_test.py
@@ -5,32 +5,34 @@ import unittest
 import input_sensor_data
 import unittest
 
-# A2D_readings = [0,0]
-# temperature_sensor_readings = [0,0]
+# # A2D_readings = [0,0]
+# # temperature_sensor_readings = [0,0]
+# # # data_received.append(sys.stdin.read())
+# # data1 = sender.start_sending(A2D_readings,temperature_sensor_readings)
+# # # print (sender.start_sending(A2D_readings,temperature_sensor_readings))
+# # print (data1)
+# # print ("----------------")
+# A2D_readings = [0,2000]
+# temperature_sensor_readings = [0,1]
 # # data_received.append(sys.stdin.read())
 # data1 = sender.start_sending(A2D_readings,temperature_sensor_readings)
 # # print (sender.start_sending(A2D_readings,temperature_sensor_readings))
-# print (data1)
-# print ("----------------")
-A2D_readings = [0,2000]
-temperature_sensor_readings = [0,1]
-# data_received.append(sys.stdin.read())
-data1 = sender.start_sending(A2D_readings,temperature_sensor_readings)
-# print (sender.start_sending(A2D_readings,temperature_sensor_readings))
-# print (data1)
-# print ("----------------")
-# A2D_readings = [4000,4000]
-# temperature_sensor_readings = [1,1]
-# # data_received.append(sys.stdin.read())
-# data1 = sender.start_sending(A2D_readings,temperature_sensor_readings)
-# # print (sender.start_sending(A2D_readings,temperature_sensor_readings))
-# print (data1)
-# class sender_test(unittest.TestCase):
+# # print (data1)
+# # print ("----------------")
+# # A2D_readings = [4000,4000]
+# # temperature_sensor_readings = [1,1]
+# # # data_received.append(sys.stdin.read())
+# # data1 = sender.start_sending(A2D_readings,temperature_sensor_readings)
+# # # print (sender.start_sending(A2D_readings,temperature_sensor_readings))
+# # print (data1)
+
+class sender_test(unittest.TestCase):
   
-#   def test(self):
-#     self.assertEqual(sender.start_sending(A2D_readings,temperature_sensor_readings), "0,32\n10,34\n" , "not equal")
+  def test(self):
+    for test_data in range (len(test_A2D_readings))
+      self.assertEqual(sender.start_sending(input_sensor_data.test_A2D_readings[test_data],(input_sensor_data.test_temperature_readings[test_data]), input_sensor_data.result_data[test_data] , "not equal")
         
-# unittest.main()
+unittest.main()
   
   
   
