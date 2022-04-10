866ca9f130af7f94c951fadca4da3a0965e9cc94
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-10 15:00:42 +0530

    Update streaming_data_test.py

--- streaming_data_test.py
+++ streaming_data_test.py
@@ -5,17 +5,18 @@ import unittest
 import input_sensor_data
 import unittest
 
-A2D_readings = [0, 4000]
-temperature_sensor_readings = [0,1]
-# data_received.append(sys.stdin.read())
-data1 = sender.start_sending(A2D_readings,temperature_sensor_readings)
-# print (sender.start_sending(A2D_readings,temperature_sensor_readings))
-print (data1)
+# A2D_readings = [0, 4000]
+# temperature_sensor_readings = [0,1]
+# # data_received.append(sys.stdin.read())
+# data1 = sender.start_sending(A2D_readings,temperature_sensor_readings)
+# # print (sender.start_sending(A2D_readings,temperature_sensor_readings))
+# print (data1)
 
 class sender_test(unittest.TestCase):
   
   def test(self):
-    self.assertEqual(sender.start_sending(A2D_readings,temperature_sensor_readings), "10,34\n10,34\n" , "not equal")
+    for x in range(len(input_sensor_data.test_A2D_readings)):
+      self.assertEqual(sender.start_sending(input_sensor_data.test_A2D_readings[x],input_sensor_data.test_temperature_readings), input_sensor_data.result_data[x] , "not equal")
         
 unittest.main()
   
