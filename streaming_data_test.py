9fb9eab801cfe611effc1de0558103c2e231fa6c
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-10 13:34:29 +0530

    Update streaming_data_test.py

--- streaming_data_test.py
+++ streaming_data_test.py
@@ -9,6 +9,6 @@ temperature_sensor_readings = [0,1]
 # data_received.append(sys.stdin.read())
 data1 = sender.start_sending(A2D_readings,temperature_sensor_readings)
 # print (sender.start_sending(A2D_readings,temperature_sensor_readings))
- print (data1)
+print (data1)
 
 
